d3c5177e21a63bba9bc675f0774af877d9515cb6
Bikram Chatterjee <56491363+i0nics@users.noreply.github.com>
2021-02-08 00:13:07 +0530

    Updated Comments

--- server.py
+++ server.py
@@ -19,13 +19,20 @@ class Thread(threading.Thread):
         self.client_s = client_s
         self.args = args
 
+    # Run thread
     def run(self):
         client_s, args = self.client_s, self.args
+
+        # Keep client socket open until client closes the connection
         while True:
             raw_bytes = bytes()
+
+            # Loop until all data from client is received
             while not raw_bytes.endswith(bytes('\r\n\r\n', 'ascii')):
                 try:
+                    # Append data to raw_bytes
                     raw_bytes += client_s.recv(args.recv)
+                    # Close socket if client sends an empty request
                     if len(raw_bytes) == 0:
                         close_s(client_s, args.verbose)
                         return
@@ -41,14 +48,22 @@ class Thread(threading.Thread):
                 print("Received %d bytes from client\n" % len(raw_bytes))
                 print("Message contents:", request.replace('\r\n\r\n', '\n'))
 
+            # Split client request message by space
             re_split = request.split(' ')
+            # Store relevant data in respective variables
             method, path, file, protocol = re_split[0], re_split[1], bytes('', 'ascii'), 'HTTP/1.1 '
+
+            # Check if client sent any unsupported requests
             status_code = '200 OK' if method == 'GET' or method == 'HEAD' else '501 Request method not implemented'
-            
+
+            # Get the file requested by client from system
             if status_code == '200 OK':
                 try:
+                    # Set requested file to HTML file containing home page if no specified path in URL is provided
                     if path == '/':
                         path = '/index.html'
+
+                    # Open requested file and generate response headers
                     with open(args.base + path, "rb") as file:
                         response_headers = {
                             'Content-Length': os.path.getsize(args.base + path),
@@ -58,16 +73,21 @@ class Thread(threading.Thread):
                             'Last-Modified': datetime.datetime.fromtimestamp(os.path.getmtime(args.base + path)).strftime('%a, %d %b %Y %H:%M:%S GMT'),
                             'Server': 'COMP 177 Python Multi-Thread\r\n'
                         }
+
+                        # Convert response headers to bytes
                         response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.items())
+                        # Send all response headers to client
                         send_data(client_s, bytes(protocol + status_code + '\r\n' + response_headers_raw, 'ascii'))
 
                         if method == 'GET':
+                            # Send a max of 64kb of the file at a time
                             while byte := file.read(64 * 1024):
                                 send_data(client_s, byte)
 
                 except (FileNotFoundError, IsADirectoryError):
                     status_code = '404 Not Found'
 
+            # Handle requests which are not supported by server or for when requested file is not found
             if status_code != '200 OK':
                 file = bytes('<html><body><center><h3 style="color:red; font-size:30px">Error ' + status_code +
                              '</h3><p>COMP 177 HTTP Server</p></center></body></html>', 'ascii')
@@ -79,11 +99,13 @@ class Thread(threading.Thread):
                 response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.items())
                 send_data(client_s, bytes(protocol + status_code + '\r\n' + response_headers_raw, 'ascii') + file)
 
+            # Close socket if client sends a "Connection: close" header
             if 'Connection: close' in request:
                 close_s(client_s, args.verbose)
                 return
 
 
+# Send relevant headers or other data to client
 def send_data(client_s, data):
     try:
         client_s.sendall(data)
